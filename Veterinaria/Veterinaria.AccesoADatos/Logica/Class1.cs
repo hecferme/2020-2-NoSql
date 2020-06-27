6dab483fa5248567d00badedf45267fec6d95e7a
trompuditasoftwarein <trompuditasoftwarein@INSTANCE-1>
2020-06-27 03:26:35 +0000

    Conexión con base de datos atlas y listado de colecciones de una base de datos

---
+++ Veterinaria/Veterinaria.AccesoADatos/Logica/Class1.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.AccesoADatos
+{
+    public class Conexion
+    {
+
+        public IMongoDatabase ConectarConBaseDeDatos ()
+        {
+            var client = new MongoClient("mongodb+srv://rwuser:12345@myatlascluster-as0q0.gcp.mongodb.net/veterinaria?retryWrites=true&w=majority");
+            var database = client.GetDatabase("veterinaria");
+            return database;
+        }
+
+        public IAsyncCursor<BsonDocument> ListarTodasLasColeciones (IMongoDatabase laBaseDeDatos)
+        {
+            var elResultado = laBaseDeDatos.ListCollections();
+            return elResultado;
+        }
+    }
+}
---
+++ Veterinaria/Veterinaria/Trabajador.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria
+{
+    public class Trabajador 
+    {
+        public void Trabajo01()
+        {
+            var laOpcion = string.Empty;
+            while (laOpcion != "X")
+            {
+                DesplegarMenu();
+                laOpcion = LeaLaOpcion();
+                switch (laOpcion)
+                {
+                    case "1":
+                        ListeLasColecciones();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ListeLasColecciones()
+        {
+            var client = new Veterinaria.AccesoADatos.Conexion();
+            var laBaseDeDatos = client.ConectarConBaseDeDatos();
+            var laListaDeColecciones = client.ListarTodasLasColeciones(laBaseDeDatos);
+
+            Console.WriteLine("Lista de colecciones in the database:");
+            foreach (var collection in laListaDeColecciones.ToList()) 
+            { 
+                Console.WriteLine(collection.ToString()); 
+            }
+        }
+
+        private void DesplegarMenu()
+        {
+            Console.WriteLine("Menu Principal");
+            Console.WriteLine("1. Listar las colecciones.");
+            Console.WriteLine("2. Listar los animalitos.");
+            Console.WriteLine("X.  Salir");
+        }
+
+        private string LeaLaOpcion()
+        {
+            string elResultado = Console.ReadLine();
+            return elResultado;
+        }
+    }
+}
