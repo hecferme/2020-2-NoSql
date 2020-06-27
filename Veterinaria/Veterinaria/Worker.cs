ca448eb3bbf15e0abe400cc3a2ad0f6bca2e519e
trompuditasoftwarein <trompuditasoftwarein@INSTANCE-1>
2020-06-27 01:22:17 +0000

    Menú principal de la aplicación

---
+++ Veterinaria/Veterinaria/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria
+{
+    public class Trabajador 
+    {
+        public void Trabajo01()
+        {
+            var laOpcion = string.Empty;
+            while (laOpcion != "X")
+            {
+                DesplegarMenu();
+                laOpcion = LeaLaOpcion();
+            }
+        }
+
+        private void DesplegarMenu()
+        {
+            Console.WriteLine("Menu Principal");
+            Console.WriteLine("1. Listar los animalitos.");
+            Console.WriteLine("X.  Salir");
+        }
+
+        private string LeaLaOpcion()
+        {
+            string elResultado = Console.ReadLine();
+            return elResultado;
+        }
+    }
+}
